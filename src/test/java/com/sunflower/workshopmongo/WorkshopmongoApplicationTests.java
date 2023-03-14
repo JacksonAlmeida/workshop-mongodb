0081193d7cde085837abdff0dc3ff656287bb2fa
Jackson Almeida <jacksonnalmeida30@gmail.com>
2023-03-14 14:02:31 -0300

    primeiro commit, projeto criado

---
+++ src/test/java/com/sunflower/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunflower.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
